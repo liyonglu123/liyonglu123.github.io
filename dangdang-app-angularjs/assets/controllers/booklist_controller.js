9062d743a2179a0d8a1185ef477329310416c9a9
liyonglu123 <1719867125@qq.com>
2016-10-19 19:31:40 +0800

    book列表展示第十次

--- dangdang-app-angularjs/assets/controllers/booklist_controller.js
+++ dangdang-app-angularjs/assets/controllers/booklist_controller.js
@@ -2,7 +2,7 @@ app.controller('bookListController',['$scope','commonService','$routeParmas',fun
   // 获取数据
   $scope.bookData =[];
 console.dir($routeParmas);
-  c_service.getData('ertong',function(res){
+  c_service.getData($routeParmas.id,function(res){
     console.log('获取数据成功');
     console.dir(res.data);
     $scope.bookData = res.data;

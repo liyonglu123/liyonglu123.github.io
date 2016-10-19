80f87ab930e51af717b40600baf69518d74c924c
liyonglu123 <1719867125@qq.com>
2016-10-19 17:06:42 +0800

    book列表的修改

--- dangdang-app-angularjs/assets/controllers/book_controller.js
+++ dangdang-app-angularjs/assets/controllers/book_controller.js
@@ -1,8 +1,8 @@
 app.controller('bookController',['$scope',function($scope){
   $scope.bookTypes = [
-    {id:'ertong',name:'儿童',img:'',description:''},
-    {id:'lishi',name:'历史',img:'',description:''},
-    {id:'dongman',name:'动漫',img:'',description:''},
-    {id:'qingchunwenxue',name:'青春文学',img:'',description:''},
+    {id:'ertong',name:'儿童',img:'http://img3x9.ddimg.cn/58/23/24026269-1_l_11.jpg',description:'当当儿童图书热搜榜'},
+    {id:'lishi',name:'历史',img:'http://img3x9.ddimg.cn/58/23/24026269-1_l_11.jpg',description:'当当历史图书热搜榜'},
+    {id:'dongman',name:'动漫',img:'http://img3x9.ddimg.cn/58/23/24026269-1_l_11.jpg',description:'当当动漫图书热搜榜'},
+    {id:'qingchunwenxue',name:'青春文学',img:'http://img3x9.ddimg.cn/58/23/24026269-1_l_11.jpg',description:'当当青春文学图书热搜榜'},
   ];
 }]);

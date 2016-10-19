a17a4054bb7634b9b6e6327fbd95314015fe20d2
liyonglu123 <1719867125@qq.com>
2016-10-19 18:09:06 +0800

    book 列表的第三次修改

--- dangdang-app-angularjs/assets/controllers/booklist_controller.js
+++ dangdang-app-angularjs/assets/controllers/booklist_controller.js
@@ -1,7 +1,7 @@
-app.controller('bookListController',['$scope','commonService',function($scope,c_service){
+app.controller('bookListController',['$scope','commonService','$routeParmas',function($scope,c_service,$routeParmas){
   // 获取数据
   $scope.bookData =[];
-
+console.dir($routeParmas);
   c_service.getData('ertong',function(res){
     console.log('获取数据成功');
     console.dir(res);

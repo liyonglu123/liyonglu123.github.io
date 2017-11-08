fedd79e325e119ec2be487febeb45e246cfae186
liyonglu123 <1719867125@qq.com>
2017-11-08 13:45:11 +0800

    资讯页面的
    资讯页面的

--- 微信小程序的测试/pages/index/index.js
+++ 微信小程序的测试/pages/index/index.js
@@ -24,6 +24,98 @@ Page({
       url: '../logs/logs'
     })
   },
+  /**
+   * 这是用户的缓存和搜索的问题
+   * 流程这么走：
+1.用户输入数据，点击搜索
+2.如果数据不为空，加入（设置）本地缓存
+3.去服务器搜索用户想要的数据，赋值给这个页面的变量
+4.点击删除，去除本地这个key的value
+这里的缓存形式的  key=>value
+var searchData = wx.getStorageSync('searchData') || []
+获取本地名字为'searchData'的缓存，如果'searchData'这个缓存不存在就相当于重新什么一个空数组，赋值给searchData这个变量
+searchData.push(this.data.inputValue)
+将用户输入的值PUSH进searchData这个变量里
+wx.setStorageSync('searchData', searchData)
+调用API接口，重新设置key = 'searchData'的这个缓存的value等于searchData
+下面的wx.request是请求数据的内容，说腻了，印象够深了。
+这里没有绑定获取缓存的bindtap，只要获取到，然后添加到Page里面的data
+   */
+  //获取用户输入框的值
+  searchNameInput:function(e){
+    var that = this;
+    that.setData({
+      inputValue:e.detail.value
+    })
+  },
+  //将用户输入的内容存入本地缓存,并且将搜索数据放到首页
+setSearchStorage:function(){
+  var that = this
+  if(this.data.inputValue != ''){
+    //调用API向本地缓存存入数据
+    var searchData = wx.getStorageSync('searchData') || [] 
+    searchData.push(this.data.inputValue) 
+    wx.setStorageSync('searchData', searchData)
+ 
+    //读取用户搜索商品
+    var name = this.data.inputValue
+    wx.request({
+     url: 'www.shop.com/home/product/search',
+     data: {name:name},
+     method: 'GET', 
+     success: function(res){
+        that.setData({
+        goodsList: res.data.info,
+      })
+     },
+    })
+  }
+},
+// //从本地获取历史搜索数据
+ 
+// var searchData = wx.getStorageSync('searchData')||[]
+
+//      this.setData({
+
+//        searchData:searchData
+
+//      })
+
+ 
+
+// // deleteHistory
+
+// //删除历史搜索数据
+
+//  deleteHistory:function(){
+
+//    var that = this
+
+//    wx.showModal({
+
+//    title: '提示',
+
+//    content: '是否删除历史搜索',
+
+//    success: function(res) {
+
+//      if (res.confirm) {
+
+//        wx.setStorageSync('searchData', []);
+
+//        wx.switchTab({
+
+//          url: '/pages/index/index',
+
+//        })
+
+//       }
+
+//      }
+
+//    })
+
+// },
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({

2cf12be930643e40ba97434efe9d092a9043e2c6
liyonglu123 <1719867125@qq.com>
2017-11-10 19:09:13 +0800

    工具类的添加的问题
    工具类的添加的问题

--- 小程序搜索/utils/util.js
+++ 小程序搜索/utils/util.js
@@ -17,3 +17,25 @@ const formatNumber = n => {
 module.exports = {
   formatTime: formatTime
 }
+
+
+var rootDocment = 'https://www.itit123.cn';
+function req(url,data,cb){
+    wx.request({
+      url: rootDocment + url,
+      data: data,
+      method: 'post',
+      header: {'Content-Type':'application/x-www-form-urlencoded'},
+      success: function(res){
+        return typeof cb == "function" && cb(res.data)
+      },
+      fail: function(){
+        return typeof cb == "function" && cb(false)
+      }
+    })
+}
+
+
+module.exports = {
+  req: req
+}

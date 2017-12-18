09aac180686da731955512daaca860a62e4c6536
liyonglu123 <1719867125@qq.com>
2017-12-18 13:47:08 +0800

    svg和hover
    svg和hover

---
+++ 前端打包/parcle/index.js
@@ -0,0 +1,5 @@
+// console.log("hello world");
+
+import main from './main';
+
+main();
---
+++ 前端打包/parcle/main.js
@@ -0,0 +1,6 @@
+//  引入一个css 模块
+import classes from './main.css';
+
+export default () => {
+  console.log(classes.main);
+};

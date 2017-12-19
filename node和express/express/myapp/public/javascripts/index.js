7dc7a97f108db4a331626e3d14e1ff00d910157d
liyonglu123 <1719867125@qq.com>
2017-12-19 17:37:49 +0800

    js 基础知识
    变量提升，区块，标签

--- node和express/express/myapp/public/javascripts/index.js
+++ node和express/express/myapp/public/javascripts/index.js
@@ -1,4 +1,4 @@
-// var template = require('art-template');
-// $('#main_panel').html(template('main_panel_big_sale_template', data));
-// var list = document.getElementById('test');
-// list.html(template('testTmp', [1,2,4]))
+
+// $('#test').html(template('testTmp', data));
+var list = document.getElementById('test');
+list.innerHTML = template('testTmp', [1,2,4]);

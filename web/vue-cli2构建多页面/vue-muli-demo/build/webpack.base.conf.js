1bed5f508399736890e5cff0a791e4bbfcdeb27f
liyonglu <liyonglu@rongyiju.com>
2019-02-20 15:00:19 +0800

    vue-cli2构建多页面
    vue-cli2构建多页面

---
+++ web/vue-cli2构建多页面/vue-muli-demo/build/webpack.base.conf.js
@@ -0,0 +1,94 @@
+'use strict'
+const path = require('path')
+const utils = require('./utils')
+const config = require('../config')
+const vueLoaderConfig = require('./vue-loader.conf')
+
+function resolve(dir) {
+    return path.join(__dirname, '..', dir)
+}
+/**
+ * 获取多页面入口文件
+ * @globPath 文件路径
+ */
+const glob = require('glob')
+
+function getEntries(globPath) {
+    const entries = glob.sync(globPath).reduce((result, entry) => {
+        const moduleName = path.basename(path.dirname(entry)) // 获取模块名称
+        result[moduleName] = entry
+        return result
+    }, {})
+    return entries
+}
+const entries = getEntries('./src/modules/**/*.js')
+module.exports = {
+    context: path.resolve(__dirname, '../'),
+    // entry: {
+    //     // app: './src/main.js' 单页面的时候
+
+    // },
+    entry: entries,
+    output: {
+        path: config.build.assetsRoot,
+        filename: '[name].js',
+        publicPath: process.env.NODE_ENV === 'production' ?
+            config.build.assetsPublicPath : config.dev.assetsPublicPath
+    },
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js',
+            '@': resolve('src'),
+        }
+    },
+    module: {
+        rules: [{
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: vueLoaderConfig
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: utils.assetsPath('img/[name].[hash:7].[ext]')
+                }
+            },
+            {
+                test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: utils.assetsPath('media/[name].[hash:7].[ext]')
+                }
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+                }
+            }
+        ]
+    },
+    node: {
+        // prevent webpack from injecting useless setImmediate polyfill because Vue
+        // source contains it (although only uses it if it's native).
+        setImmediate: false,
+        // prevent webpack from injecting mocks to Node native modules
+        // that does not make sense for the client
+        dgram: 'empty',
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty',
+        child_process: 'empty'
+    }
+}

acaa5d66fa652a61e6f123a9d1aefb436276bb43
liyonglu <liyonglu@rongyiju.com>
2019-09-04 17:29:16 +0800

    删除无用文件
    删除无用文件

--- 谷歌js插件开发/popup.js
+++
@@ -1,7 +0,0 @@
-function sayHello() {
-    var message = document.createTextNode("Hello World!");
-    var out = document.createElement("div");
-    out.appendChild(message);
-    document.body.appendChild(out);
-}
-window.onload = sayHello;

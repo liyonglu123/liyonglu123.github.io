9191bb8b46b759be3f6a1dd00c5a5a021a988d34
liyonglu <liyonglu@rongyiju.com>
2019-11-15 10:27:07 +0800

    gulp新增
    gulp新增

--- gulp-demo/gulpfile.js
+++ gulp-demo/gulpfile.js
@@ -3,28 +3,34 @@ const plugins = require('gulp-load-plugins')();
 const uglify = require("gulp-uglify");
 const less = require("gulp-less");
 let browserSync = require('browser-sync').create(); // 浏览器自动刷新
+const del  = require('del');
 // js
 function js(cb) {
-    return src("src/js/*.js")
+    src("src/js/*.js")
         .pipe(plugins.uglify())
-        .pipe(dest("dist"));
+        .pipe(dest("dist"))
     cb();
+    browserSync.reload();
 }
 
 // css 
 function style(cb) {
-    return src("src/css/*.less")
+    src("src/css/*.less")
         .pipe(less())
-        .pipe(dest("dist"));
+        .pipe(dest("dist"))
     cb();
+    browserSync.reload();
+    
 }
 
 
 // html
 function html(cb) {
-    return src("src/*.html")
+    src("src/*.html")
         .pipe(dest("dist"))
     cb();
+    browserSync.reload();
+
 }
 
 // 图片
@@ -34,12 +40,24 @@ function images(cb) {
 
 // 清楚文件
 function cleanDir(cb) {
-    return src("./dist")
-        .pipe(plugins.clean())
+    del("./dist");
     cb();
 }
 
-
+// 监听文件变化
+function watchDir(cb) {
+    watch("src/js/*.js", js);
+    watch("src/css/*.less", style);
+    watch("src/*.html", html);
+    cb();
+}
+// 浏览器自动刷新
+function serve(cb) {
+    browserSync.init({
+        baseDir: "src"
+    });
+    cb();
+}
 // 默认任务
 function defaultTask(cb) {
     cb();
@@ -51,5 +69,7 @@ exports.default = series(
     js,
     style,
     html,
+    watchDir,
+    serve,
     defaultTask
 );
--- gulp-demo/src/js/index.js
+++ gulp-demo/src/js/index.js
@@ -1,2 +1,2 @@
-console.log("hello");
-console.log("hello111fwegwegwe");
+console.log("hello烦都烦死地方gwegweg");
+console.log("hello111fwegwegwe淡淡的非斯蒂芬斯蒂芬斯蒂芬");

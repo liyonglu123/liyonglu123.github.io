062ebc9ce2f18809a5d6baf375d487a544a3c240
liyonglu <1719867125@qq.com>
2020-04-05 12:34:00 +0800

    算法和数据结构
    算法和数据结构

--- leetcode/code/sum.js
+++ leetcode/code/sum.js
@@ -2,3 +2,41 @@ function sum(a, b) {
     return a + b;
 }
 export default sum;
+
+// function fn() {
+//     getValue = function(){
+//         console.log(1)
+//     }
+//     return this
+// }
+// fn.getValue = function() {
+//     console.log(2)
+// }
+// fn.prototype.getValue = function() {
+//     console.log(3)
+// }
+// var getValue = function () {
+//     console.log(4)
+// }
+// function getValue() {
+//     console.log(5)
+// }
+// getValue() 4
+// fn().getValue() 1
+// getValue() 1 
+// new fn.getValue() 2
+// new fn().getValue() 3
+
+
+// var a = 10
+// var foo = {
+//     a: 20,
+//     bar: function() {
+//         var a = 30
+//         return this.a
+//     }
+// }
+// console.log(foo.bar())  20 
+// console.log((foo.bar)()) 20 
+// console.log((foo.bar = foo.bar)()) 10 
+// console.log((foo.bar, foo.bar)()) 10

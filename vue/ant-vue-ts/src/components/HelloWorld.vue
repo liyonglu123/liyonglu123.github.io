294b6590861dae930e1b19ed75fd1ea265c2eae5
liyonglu <18333159089@163.com>
2020-04-21 22:01:48 +0800

    ant-vue-ts的基本搭建
    ant-vue-ts的基本搭建

---
+++ vue/ant-vue-ts/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="blogpost">
+		<h2>{{ post.title }}</h2>
+		<p>{{ post.body }}</p>
+		<p class="meta">Written by {{ post.author }} on {{ date }}</p>
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+// 在这里对数据进行类型约束
+export interface Post {
+	title: string;
+	body: string;
+	author: string;
+	datePosted: Date;
+}
+
+@Component
+export default class HelloWorld extends Vue {
+	@Prop() private post!: Post;
+
+	get date() {
+		return `${this.post.datePosted.getDate()}/${this.post.datePosted.getMonth()}/${this.post.datePosted.getFullYear()}`;
+	}
+}
+</script>
+
+<style scoped>
+h2 {
+  text-decoration: underline;
+}
+p.meta {
+  font-style: italic;
+}
+</style>

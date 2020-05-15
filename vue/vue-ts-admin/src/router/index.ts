61f864c53ad61ebd0f0c39b4e3ea4fd9f93b9c05
liyonglu <liyonglu@100tal.com>
2020-05-15 16:37:36 +0800

    vue-ts-admin
    vue-ts-admin

--- vue/vue-ts-admin/src/router/index.ts
+++ vue/vue-ts-admin/src/router/index.ts
@@ -14,21 +14,21 @@ const routes: Array<RouteConfig> = [
       next()
     }
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () =>
-  //     import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // },
-  // {
-  //   path: '/login',
-  //   name: 'Login',
-  //   component: () =>
-  //     import(/* webpackChunkName: "about" */ '@/views/login/index')
-  // }
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () =>
+      import(/* webpackChunkName: "about" */ '@/views/login/index')
+  }
 ]
 
 const router = new VueRouter({

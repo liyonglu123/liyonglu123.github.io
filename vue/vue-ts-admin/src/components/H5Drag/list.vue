946eb80eeecec8263955c90c00f396498e09f70a
liyonglu <liyonglu@100tal.com>
2020-05-22 13:40:11 +0800

    h5-drag-list 添加
    h5-drag-list 添加

--- vue/vue-ts-admin/src/components/H5Drag/list.vue
+++ vue/vue-ts-admin/src/components/H5Drag/list.vue
@@ -2,13 +2,14 @@
   <div class="drag-list-container">
     <div
       class="drag-item"
+      :class="'drag-item-' + index"
       draggable="true"
       v-for="(item, index) in dragList"
       :key="index"
-      @dragstart="dragstart($event)"
-      @dragend="dragend($event)"
-      @dragover="dragover($event)"
-      @drop="drop($event)"
+      @dragstart="dragstart($event, item, index)"
+      @dragend="dragend($event, item, index)"
+      @dragover="dragover($event, item, index)"
+      @drop="drop($event, item, index)"
     >
       {{ item.name }}
     </div>
@@ -25,6 +26,9 @@ import { forEach } from 'lodash'
 export default class DragList extends Vue {
   public dragList: IdragItem[] = []
   protected list: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  protected dragItemIndex?: number
+  protected dragItem?: object
+
   mounted() {
     this.setDragList()
   }
@@ -35,8 +39,28 @@ export default class DragList extends Vue {
       })
     })
   }
-  dragstart(event) {
-    console.log()
+  swapItem(fromIndex: number, toIndex: number) {
+    console.log(fromIndex, toIndex)
+    const toItem = this.dragList[toIndex]
+    this.$set(this.dragList, toIndex, this.dragItem)
+    this.$set(this.dragList, fromIndex, toItem)
+    console.log(this.dragList)
+  }
+  dragstart(event: DragEvent, item: IdragItem, index: number) {
+    this.dragItem = item
+    this.dragItemIndex = index
+  }
+  dragend(event: DragEvent, item: IdragItem, index: number) {
+    console.log('dragend', item, index)
+  }
+  dragover(event: DragEvent, item: IdragItem, index: number) {
+    event.preventDefault()
+  }
+  drop(event: DragEvent, item: IdragItem, index: number) {
+    console.log('drop', item, index)
+    if (this.dragItemIndex) {
+      this.swapItem(this.dragItemIndex, index)
+    }
   }
 }
 </script>

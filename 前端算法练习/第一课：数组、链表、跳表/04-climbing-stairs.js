525e5233afad651081eae152262d6ccae2388763
liyonglu <liyonglu@100tal.com>
2021-09-07 20:00:59 +0800

    爬楼梯
    爬楼梯

---
+++ 前端算法练习/第一课：数组、链表、跳表/04-climbing-stairs.js
@@ -0,0 +1,44 @@
+// https://leetcode-cn.com/problems/climbing-stairs/
+/**
+ * 枚举，数学归纳法
+ * 边界条件
+ * 1： 1
+ * 2： 2
+ * 3： 1 + 2 = 3  f(3) = f(1) + f(2)
+ */
+// 1. 有很多重复的计算
+var climbStairs = function (n) {
+  if (n === 1) {
+    return 1;
+  }
+  if (n === 2) {
+    return 2;
+  }
+  if (n > 2) {
+    return climbStairs(n - 1) + climbStairs(n - 2);
+  }
+};
+
+// 有效的只是相邻的三个元素
+var climbStairs = function (n) {
+  var prev = 0;
+  var cur = 1;
+  var temp;
+  for (var i = 0; i < n; i++) {
+    temp = prev;
+    prev = cur;
+    cur += temp;
+  }
+  return cur;
+};
+
+// 动态规划
+var climbStairs = function (n) {
+  var dp = [];
+  dp[1] = 1;
+  dp[2] = 2;
+  for (var i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
+};

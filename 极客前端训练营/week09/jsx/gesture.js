cab332af48b0c5acdf87429b12b6a9f1cfcf7882
liyonglu123 <1719867125@qq.com>
2021-11-07 23:05:35 +0800

    feat: 手势和动画 | 实现一个 Flick 事件
    手势和动画 | 实现一个 Flick 事件

--- 极客前端训练营/week09/jsx/gesture.js
+++ 极客前端训练营/week09/jsx/gesture.js
@@ -87,6 +87,8 @@ let start = (point, context) => {
   //   console.log("start", point.clientX, point.clientY);
   context.startX = point.clientX;
   context.startY = point.clientY;
+  // 存储点， 用于速度计算，平均值
+  context.points = [{ t: Date.now(), x: point.clientX, y: point.clientY }];
   context.isTap = true;
   context.isPan = false;
   context.isPress = false;
@@ -113,6 +115,9 @@ let move = (point, context) => {
     console.log(dx, dy);
     console.log("pan");
   }
+  // 过滤, 只存取半秒内的点
+  context.points = context.points.filter((point) => Date.now() - point.t < 500);
+  context.points.push({ t: Date.now(), x: point.clientX, y: point.clientY });
   //   console.log("move", point.clientX, point.clientY);
 };
 let end = (point, context) => {
@@ -127,6 +132,26 @@ let end = (point, context) => {
   if (context.isPress) {
     console.log("pressend");
   }
+  context.points = context.points.filter((point) => Date.now() - point.t < 500);
+  let d, v;
+  if (!context.points.length) {
+    v = 0;
+  } else {
+    // console.log(context, context.points);
+    d = Math.sqrt(
+      (point.clientX - context.points[0].x) ** 2 +
+        (point.clientY - context.points[0].y) ** 2
+    );
+    v = d / (Date.now() - context.points[0].t);
+  }
+  if (v > 1.5) {
+    console.log("flick");
+    context.isFlick = true;
+  } else {
+    context.isFlick = false;
+  }
+
+  console.log(v);
   //   console.log("end", point.clientX, point.clientY);
 };
 let cancel = (point, context) => {

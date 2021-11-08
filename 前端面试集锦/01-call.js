bd4f3e0d82ed690b8d819cd636d4082e7abcb51c
liyonglu123 <1719867125@qq.com>
2021-11-08 10:21:46 +0800

    feat: 手写call的补充
    feat: 手写call的补充

--- 前端面试集锦/01-call.js
+++ 前端面试集锦/01-call.js
@@ -82,6 +82,18 @@ Function.prototype.myCall = function (context) {
 // greet.myCall(obj, 1, 2, 3);
 // console.log(obj);
 
+Function.prototype.myCall = function (thisArg, ...args) {
+  const fn = Symbol("fn"); // 声明一个独有的Symbol属性, 防止fn覆盖已有属性
+  thisArg = thisArg || window; // 若没有传入this, 默认绑定window对象
+  thisArg[fn] = this; // this指向调用call的对象,即我们要改变this指向的函数
+  const result = thisArg[fn](...args); // 执行当前函数
+  delete thisArg[fn]; // 删除我们声明的fn属性
+  return result; // 返回函数执行结果
+};
+
+//测试
+foo.myCall(obj); // 输出'写代码像蔡徐抻'
+
 // ------ 经典面试题 --------
 // 一个call了是让左边的函数执行，多个call是让传参的函数执行(this是window/undefined)
 function fn1() {

5606e8ca050cef372e3477b4454f72ca7d8a5033
liyonglu123 <1719867125@qq.com>
2021-11-08 10:26:02 +0800

    feat: 手写bind的补充
    feat: 手写bind的补充

--- 前端面试集锦/03-bind.js
+++ 前端面试集锦/03-bind.js
@@ -32,6 +32,30 @@ Function.prototype.myBind = function (obj) {
   return bindFn;
 };
 
+Function.prototype.myBind = function (thisArg, ...args) {
+  var self = this;
+  // new优先级
+  var fbound = function () {
+    self.apply(
+      this instanceof self ? this : thisArg,
+      args.concat(Array.prototype.slice.call(arguments))
+    );
+  };
+  // 继承原型上的属性和方法
+  fbound.prototype = Object.create(self.prototype);
+
+  return fbound;
+};
+
+//测试
+const obj = { name: "写代码像蔡徐抻" };
+function foo() {
+  console.log(this.name);
+  console.log(arguments);
+}
+
+foo.myBind(obj, "a", "b", "c")(); //输出写代码像蔡徐抻 ['a', 'b', 'c']
+
 // 测试
 
 var obj = {

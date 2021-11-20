e1076069c3571eefe6366885a6b9de1ad584ff9c
liyonglu123 <1719867125@qq.com>
2021-11-20 11:19:58 +0800

    feat: 初始化与构建 Yeoman | 二

--- 极客前端训练营/week11/README.md
+++ 极客前端训练营/week11/README.md
@@ -1,3 +1,6 @@
 # 工具链
 
 1. 初始化与构建 Yeoman | 一
+2. 初始化与构建 Yeoman | 二
+   - 生成 demo
+   - extendJSON 安装依赖
--- 极客前端训练营/week11/toolsChain/generators/app/index.js
+++ 极客前端训练营/week11/toolsChain/generators/app/index.js
@@ -7,13 +7,45 @@ module.exports = class extends Generator {
     super(args, opts);
 
     // Next, add your custom code
-    this.option("babel"); // This method adds support for a `--babel` flag
+    // this.option("babel"); // This method adds support for a `--babel` flag
   }
-  method1() {
-    this.log("method 1 just ran");
+  // async method1() {
+  //   const answers = await this.prompt([
+  //     {
+  //       type: "input",
+  //       name: "name",
+  //       message: "Your project name",
+  //       default: this.appname, // Default to current folder name
+  //     },
+  //     {
+  //       type: "confirm",
+  //       name: "cool",
+  //       message: "Would you like to enable the Cool feature?",
+  //     },
+  //   ]);
+  //   this.log("method 1 just ran");
+  //   this.log("app name", answers.name);
+  //   this.log("cool feature", answers.cool);
+  // }
+  async step1() {
+    this.fs.copyTpl(
+      this.templatePath("t.html"),
+      this.destinationPath("public/index.html"),
+      { title: "Templating with Yeoman" }
+    );
   }
 
-  method2() {
-    this.log("method 2 just ran");
+  initPackage() {
+    const pkgJson = {
+      devDependencies: {
+        eslint: "^3.15.0",
+      },
+      dependencies: {
+        react: "^16.2.0",
+      },
+    };
+    // Extend or create package.json file in destination path
+    this.fs.extendJSON(this.destinationPath("package.json"), pkgJson);
+    // this.npmInstall(); this.npmInstall is not a function， 初步判断是extendJSON的时候就已经安装了依赖
   }
 };

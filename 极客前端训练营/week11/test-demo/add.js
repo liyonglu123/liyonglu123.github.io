258528ca41c4f921313834e654c9433384a82c11
liyonglu123 <1719867125@qq.com>
2021-11-21 16:58:48 +0800

    feat: 单元测试工具 | Mocha(二)

--- 极客前端训练营/week11/test-demo/add.js
+++ 极客前端训练营/week11/test-demo/add.js
@@ -1,5 +1,8 @@
-function add(a, b) {
+// mocha --require @babel/register 找不到全局的问题
+// ./node_modules/.bin/mocha --requrie @babel/register 报错找不到需要转化的格式 config
+// .babelrc   ----->  @babel/preset-env
+export function add(a, b) {
   return a + b;
 }
 
-module.exports = add;
+// module.exports = add;
--- 极客前端训练营/week11/test-demo/test/test.js
+++ 极客前端训练营/week11/test-demo/test/test.js
@@ -1,5 +1,7 @@
 var assert = require("assert");
-var add = require("../add.js");
+// var add = require("../add.js");
+import { add } from "../add.js";
+
 describe("add function testing", function () {
   it("1 + 2 shoule be 3", function () {
     assert.equal(add(1, 2), 3);
